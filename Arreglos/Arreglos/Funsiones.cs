c58b7403223bb58f60da844ed0e9a26aef7319b6
Tchandra-17 <tchandra.camacho193@tectijuana.edu.mx>
2019-11-05 09:07:06 -0800

    Arreglos

--- Arreglos/Arreglos/Funsiones.cs
+++ Arreglos/Arreglos/Funsiones.cs
@@ -8,9 +8,19 @@ namespace Arreglos
 {
     class Funsiones
     {
-        public static int ejercicio1()
+        public static void ejercicio1()
         {
-
+            Console.WriteLine("\n******* Ejercicio 1 *******");
+            int numeros;
+            int[] array = new int[numeros];
+            for (int i=0; i<=14; i++)
+            {
+                if (i >= 4)
+                {
+                    array[i] = i;
+                    Console.WriteLine(array[i]);
+                }
+            }
         }
     }
 }
