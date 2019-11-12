48554ac87228d43ebc4f0a082df8eae19f08db94
Tchandra-17 <tchandra.camacho193@tectijuana193.edu.mx>
2019-11-12 09:01:35 -0800

    Cambio

--- Matricez/Matricez/Program.cs
+++ Matricez/Matricez/Program.cs
@@ -10,7 +10,41 @@ namespace Matricez
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Bienvenido. \n"
+                + "Tenemos una serie de opciones a realizar: \n"
+                + "\t1) Empleados y sueldo. \n"
+                + "\t2) Formar la letra R en una matriz de NxN. \n"
+                + "\t3) Número mayor y menor en una matriz de NxN. \n"
+                + "\t4) Ordenamiento de una matriz de NxN. \n"
+                + "\t5) Menores de cada columna de una matriz NxN en un vector. \n"
+                + "\t6) Mayores de cada fila de una matriz NxN en un vector. \n"
+                + "¿Cuál deseas realizar?");
+            int opc = int.Parse(Console.ReadLine());
+            switch (opc)
+            {
+                case 1:
+                    Matricez.ejercicio1();
+                    break;
+                case 2:
+                    Matricez.ejercicio2();
+                    break;
+                case 3:
+                    Matricez.ejercicio3();
+                    break;
+                case 4:
+                    Matricez.ejercicio4();
+                    break;
+                case 5:
+                    Matricez.ejercicio5();
+                    break;
+                case 6:
+                    Matricez.ejercicio6();
+                    break;
+                default:
+                    Matricez.otra_opcion();
+                    break;
+            }
+            Console.ReadKey();
         }
     }
 }
