4bd8b6ddeee10b277df4e58ecf94b14d373ec06a
Tchandra-17 <tchandra.camacho193@tectijuana193.edu.mx>
2019-11-12 09:08:09 -0800

    Cambio

---
+++ Matricez/Matricez/Matricez.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matricez
+{
+    class Matricez
+    {
+        public static void ejercicio1()
+        {
+            string[] empleados = new string[4];
+            int[,] sueldos = new int[4, 3];
+
+
+        }
+        public static void ejercicio2()
+        {
+
+        }
+        public static void ejercicio3()
+        {
+
+        }
+        public static void ejercicio4()
+        {
+
+        }
+        public static void ejercicio5()
+        {
+
+        }
+        public static void ejercicio6()
+        {
+
+        }
+        public static void otra_opcion()
+        {
+            // Abre una nueva ventana donde iniciar otra vez el programa.
+            System.Diagnostics.Process.Start(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            //Cierra el programa actual.
+            Environment.Exit(0);
+        }
+    }
+}
